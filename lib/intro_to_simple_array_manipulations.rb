873facc64c71de46172a3f62b7efe621df3313c3
wengong <wentoshare@gmail.com>
2016-03-07 20:28:16 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,55 @@
+def using_push(a,b)
+  a.push(b)
+end
+def using_unshift(a,b)
+  a.unshift(b)
+end
+def using_unshift(a,b)
+  a.unshift(b)
+end
+def using_pop(a)
+  a.pop
+end
+def using_pop(a)
+  a.pop
+end
+def pop_with_args(a)
+  a.pop(2)
+
+end
+
+def using_shift(a)
+  a.shift
+end
+
+def shift_with_args(a)
+  a.shift(2)
+end
+
+def using_concat(a,b)
+ a.concat(b)
+end
+
+def using_insert(a,b)
+  a.insert(3,b)
+end
+
+def using_insert(a,b)
+  a.insert(4,b)
+end
+
+def using_uniq(a)
+  a.uniq
+end
+
+def using_flatten(a)
+  a.flatten
+end
+
+def using_delete(a,b)
+  a.delete(b)
+end
+
+def using_delete_at(a,b)
+  a.delete(a[b])
+end
